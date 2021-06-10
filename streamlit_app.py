9fb3f2b562056c1c4dbf4d84bbef72800d838650
Vikas Gupta <7848248+vikasgupta-github@users.noreply.github.com>
2021-06-10 18:12:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,56 @@
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
-import cv_app
+import cv2
+from PIL import Image
+import numpy as np
 
-st.title("OpenCV Webinar Series")
-st.header("Digit Recognizer using OpenCV and Streamlit")
-st.write("Draw a digit over the canvas and click Predict")
+st.title("OpenCV Deep Learning based Face Detection")
+modelFile = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
+configFile = "deploy.prototxt"
+net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
 
-# Create a canvas component
-canvas_result = st_canvas(
-    stroke_color="#fff",
-    background_color="#000",
-    height=250,
-    width=250,
-    key="canvas",
-)
+def col(n):
+    x = [1 for _ in range(n)]
+    c  = st.beta_columns(x)
+    return c
 
-if st.button("predict"):
-    classId, confidence = cv_app.predict(canvas_result)
-    st.success("Predicted Class: {}, Confidence: {:.2f}%".format(classId, confidence*100.))
+def detectFaceOpenCVDnn(net, frame, framework="caffe", conf_threshold=0.5):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
 
+    blob = cv2.dnn.blobFromImage(
+        frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], False, False,
+    )
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv2.rectangle(
+                frameOpencvDnn,
+                (x1, y1),
+                (x2, y2),
+                (0, 255, 0),
+                int(round(frameHeight / 150)),
+                8,
+            )
+    return frameOpencvDnn, bboxes
+
+
+uploaded_file = st.file_uploader("Choose a file", type =['jpg','jpeg','jfif','png'])
+if uploaded_file is not None:
+    image = np.array(Image.open(uploaded_file))
+    image = cv2.resize(image, (350,350))
+    
+    placeholders = col(2)
+    placeholders[0].image(image)
+    conf_threshold = st.slider("SET Confidence Threshold", min_value = 0.01, max_value = 1.0, step = .01, value=0.5)
+    out_image,_ = detectFaceOpenCVDnn(net, image, conf_threshold=conf_threshold)
+    
+    placeholders[1].image(out_image)
